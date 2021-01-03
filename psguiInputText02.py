a5ab253ebcfd54e3c2463f89048f922f284eb69b
tatsu1704 <tatsushi.k.1204@gmail.com>
2021-01-03 11:38:20 +0900

    テキストの入力に関してその２

---
+++ psguiInputText02.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import PySimpleGUI as sg
+
+layout = [[sg.InputText(default_text='ここで文字列を編集',
+                       size=(35,1), 
+                       enable_events=True, key='tx1')],
+        [sg.Button('read', key='btn1')]]
+
+
+window = sg.Window('psguiInputText01', layout)
+
+while True:
+    event, values = window.read()
+    print('イベント:', event,', 値:', values)
+    if event == None:
+        break
+
+window.close()
+
